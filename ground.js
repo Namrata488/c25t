2ae8149c2c1e594ab1ee9fe26d6eb40f12e95ba5
Namrata488 <76195975+Namrata488@users.noreply.github.com>
2021-02-14 16:43:19 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,31 @@
+class ground
+{
+	constructor(x,y,w,h)
+	{
+		var options={
+			isStatic:true			
+			}
+		this.x=__;
+		this.y=__;
+		this.w=__
+		this.h=__
+		this.body=Bodies.rectangle(___________);
+ 		World.add(______________);
+
+	}
+	display()
+	{
+			
+			var groundPos=this.body.position;		
+
+			push()
+			translate(_________________);
+			rectMode(__________)
+			//strokeWeight(4);
+			fill(128,128,128)
+			rect(0,0,____,____);
+			pop()
+			
+	}
+
+}
